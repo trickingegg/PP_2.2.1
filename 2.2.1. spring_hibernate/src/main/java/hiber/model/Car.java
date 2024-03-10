4fe79586bb4d198d6cd7b2146b00dee461aed44b
Andrew Bogatyryov <trickingegg@gmail.com>
2024-03-10 10:02:03 +0300

    убрал лишнее

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -27,14 +27,6 @@ public class Car {
         this.series = series;
     }
 
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-
     public String getModel() {
         return model;
     }
@@ -51,11 +43,4 @@ public class Car {
         this.series = series;
     }
 
-//    public User getUser() {
-//        return user;
-//    }
-//
-//    public void setUser(User user) {
-//        this.user = user;
-//    }
 }
