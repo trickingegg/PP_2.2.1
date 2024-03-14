49cccf3f6486b47d5d6871149ff7e9a4506abf00
Andrew Bogatyryov <trickingegg@gmail.com>
2024-03-14 08:10:51 +0300

    - теперь findUserByCar и listUsers не генерируют несколько запросов подряд

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -24,14 +24,14 @@ public class UserDaoImp implements UserDao {
    @Override
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("select distinct u from User u left join fetch u.car", User.class);
       return query.getResultList();
    }
 
    @Override
    public User findUserByCar(String model, int series) {
       try (Session session = sessionFactory.openSession()) {
-         Query<User> query = session.createQuery("from User where car.model = :model and car.series = :series");
+         Query<User> query = session.createQuery("select distinct u from User u join fetch u.car c where c.model = :model and c.series = :series", User.class);
          query.setParameter("model", model);
          query.setParameter("series", series);
          return query.uniqueResult();
